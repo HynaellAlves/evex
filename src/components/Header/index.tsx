5a15d02e47cdad0c147fad47ddb4fc65a8f9e352
Hynaell Alves <hynaell@gmail.com>
2025-07-15 15:19:11 -0300

    feat: Adicionando header | Ajustando componente de imagem

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,1 +1,17 @@
+// Importando módulo de estilo
 import styles from './header.module.css'
+
+// Importando Imagem componente
+import Image from "@/components/Image";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <Image href='/' class={styles.logo_main} src="/logo_main.png" width={281} height={106} />
+            <div className={styles.header_icons}>
+                <Image href='/Help' class={styles.header_icon} src="/accessibility_tools_icon.svg" width={46} height={46} />
+                <Image href='/Acessibility' class={styles.header_icon} src="/help_icon.svg" width={46} height={46} />
+            </div>
+        </header>
+    )
+}
--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,19 +1,18 @@
 import Img from "next/image";
-{/*  falta pegar a imagem, esperando design */}
+
 type imageProps = {
-    id: string
     src: string;
+    href?: string;
     alt?: string;
-    width?: number;
-    height?: number;
-    className?: string;
+    width: number;
+    height: number;
+    class: string;
 }
 
 export default function Image(props: imageProps) {
     return (
-        <div id = {props.id} className={props.className}>
-            {/*  No src colocar a regra de se o caminho estiver */}
-            <Img style={{ maxWidth: props.width, maxHeight: props.height }} src={props.src || '/img_empty'} alt={props.alt || "Image"} width={props.width} height={props.height} />
+        <div style={{ maxHeight: props.height, maxWidth: props.width }} className={props.class}>
+            <a href={props.href}><Img style={{ maxHeight: props.height }} src={props.src || '/img_empty.png'} alt={props.alt || "Image"} width={props.width} height={props.height} /></a>
         </div>
     );
 }
