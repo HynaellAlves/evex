565946418c5ee1e7f6218543c6a9cc8b6fc6c131
Hynaell Alves <hynaell@gmail.com>
2025-07-21 15:09:46 -0300

    feat: recovery

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -3,7 +3,7 @@ import { user } from '@/propierts/types';
 
 const BASE_URL = process.env.BASE_URL_API;
 
-export async function auth(user: user) {
+export async function authLogin(user: user) {
 
         if (BASE_URL) {
 
@@ -57,45 +57,3 @@ export async function auth(user: user) {
             }
         }
 }
-
-// export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-
-//     if (req.method === 'POST') {
-
-//         if (BASE_URL) {
-
-//             const { email, password } = req.body;
-
-//             try {
-
-//                 const response = await axios.post(`${BASE_URL}auth/login`, { email, password });
-//                 return res.status(response.status).json(response.data)
-
-//             } catch (err: any) {
-
-                
-//                 if (err.response) {
-//                     const status = err.response.status;
-//                     const data = err.response.data
-
-//                     console.log(`Erro na requisição da API externa: Código: ${status} - ${data}`);
-
-//                     return res.status(status).json(data.message ? data.message : data);
-
-//                 } else {
-
-//                     console.error("Erro inesperado na requisição:", err);
-
-//                     return res.status(500).json({ message: 'Erro interno no servidor' });
-//                 }
-//             }
-
-//         } else {
-//             return res.status(500).json('BASE_URL não definida')
-//         }
-//     } else {
-//         return res.status(405).json('Método não permitido')
-//     }
-// }
-
-
