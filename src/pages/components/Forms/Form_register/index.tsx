e4446fcd25b376555f3e885a486bc025c81b8a19
JoaoFerraz06 <joaoferrazg08@gmail.com>
2025-07-23 10:43:59 -0300

    feat: update OwnerPage layout and add form styles

--- src/pages/components/Forms/Form_register/index.tsx
+++ src/pages/components/Forms/Form_register/index.tsx
@@ -28,7 +28,20 @@ export default function Form() {
         <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
 
             <div className={styles.title_content}>
-                <Title class={styles.title} title="Cadastrar Usuário" fontFamily='var(--font-poppins)' fontWeight={700} />
+                <Title class={styles.title} title="Cadastrar Usuário" fontFamily='var(--font-poppins)' fontWeight={700} uppercase />
+            </div>
+
+            <div className={styles.formProgess_content}>
+                    <div className={`${styles.circles} ${styles.progess_on}`}><p>1</p></div>
+                    <div className={`${styles.progess_bar} ${styles.progess_off}`}></div>
+                    <div className={`${styles.circles} ${styles.progess_off}`}><p>2</p></div>
+                    <div className={`${styles.progess_bar} ${styles.progess_off}`}></div>
+                    <div className={`${styles.circles} ${styles.progess_off}`}><p>3</p></div>
+            </div>
+
+            <div className={styles.Progess_content}>
+                <div id={styles.box_1}> <h2>Administrador</h2>  </div>
+                <div id={styles.box_2}> <h2>Dono do evento</h2> </div>
             </div>
 
             <div className={styles.inputs_container}>
@@ -40,13 +53,15 @@ export default function Form() {
                     name="nome"
                     autoComplete="name"
                 />
+
+                <div className={styles.inputs_row}>
                 <Input
                     {...register("dataNascimento")}
                     type="date"
                     placeholder={errors.dataNascimento ? "Data obrigatória" : "Data de nascimento"}
                     className={errors.dataNascimento ? styles.input_error : styles.input_ok}
                     name="dataNascimento"
-                />
+                    />
                 <Input
                     {...register("email")}
                     type="email"
@@ -54,7 +69,10 @@ export default function Form() {
                     className={errors.email ? styles.input_error : styles.input_ok}
                     name="email"
                     autoComplete="username"
-                />
+                    />
+                </div>
+
+                <div className={styles.inputs_row}>
                 <Input
                     {...register("cep")}
                     type="text"
@@ -62,14 +80,16 @@ export default function Form() {
                     className={errors.cep ? styles.input_error : styles.input_ok}
                     name="cep"
                     maxLength={8}
-                />
+                    />
                 <Input
                     {...register("numero")}
                     type="text"
                     placeholder={errors.numero ? "Número inválido" : "Nº"}
                     className={errors.numero ? styles.input_error : styles.input_ok}
                     name="numero"
-                />
+                    />
+                </div>
+
                 <Input
                     {...register("complemento")}
                     type="text"
@@ -77,13 +97,15 @@ export default function Form() {
                     className={styles.input_ok}
                     name="complemento"
                 />
+
+                <div className={styles.inputs_row}>
                 <Input
                     {...register("cpfCnpj")}
                     type="text"
                     placeholder={errors.cpfCnpj ? "CPF/CNPJ inválido" : "CPF/CNPJ"}
                     className={errors.cpfCnpj ? styles.input_error : styles.input_ok}
                     name="cpfCnpj"
-                />
+                    />
                 <Input
                     {...register("telefone")}
                     type="tel"
@@ -91,7 +113,9 @@ export default function Form() {
                     className={errors.telefone ? styles.input_error : styles.input_ok}
                     name="telefone"
                     autoComplete="tel"
-                />
+                    />
+                </div>
+
                 <Input
                     {...register("fotoPerfil")}
                     type="url"
@@ -100,11 +124,33 @@ export default function Form() {
                     name="fotoPerfil"
                     autoComplete="url"
                 />
+
+                <div className={styles.checkboxs_container}>
+
+                    <div className={styles.checkbox_content}>
+                        <input id={styles.checkbox} type="checkbox" name="remember" />
+                        <p className={styles.label_checkbox}>Senha padrão</p>
+                    </div>
+
+                    <div className={styles.checkbox_content}>
+                        <input id={styles.checkbox} type="checkbox" name="remember" />
+                        <p className={styles.label_checkbox}>Criar senha</p>
+                    </div>
+
+                    <div className={styles.checkbox_content}>
+                        <input id={styles.checkbox} type="checkbox" name="remember" />
+                        <p className={styles.label_checkbox}>Usuário cria a própia senha</p>
+                    </div>
+
+                </div>
+
+
             </div>
 
             <div className={styles.button_content}>
                 <Button type="submit" text="avançar" />
             </div>
+
         </form>
     )
 }
