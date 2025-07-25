5434257294e5d19cd8bb3e694bb6c2fcfd71e0c6
EVEX <evex.noreply@gmail.com>
2025-07-24 21:52:50 -0300

    fix: Debug #16

--- src/pages/components/Forms/Form_login/index.tsx
+++ src/pages/components/Forms/Form_login/index.tsx
@@ -7,7 +7,7 @@ import React, { useEffect, useState } from "react";
 import Input from "@/pages/components/Input/Input_default";
 import Title from "@/pages/components/Title";
 import Button from "@/pages/components/Buttons/Button_default"
-import { toast_sucess, toast_error, toast_loading } from "../../Toast/toast";
+import showToast from "@/pages/components/Toast/toast"
 
 import styles from "./form.module.css";
 
@@ -34,7 +34,7 @@ export default function Form() {
 
   async function onSubmit(data: any) {
 
-    toast_loading("Carregando...");
+    showToast("Carregando...", "info")
 
     const response = await Login(data);
     setResponse(response);
@@ -46,83 +46,83 @@ export default function Form() {
 
       if (response.status) {
 
-        toast_error(`Erro de Login ${response.data}`);
+        showToast(`Erro de Login ${response.data}`, "error")
 
-      } else if (response.permissions) {
+    } else if (response.permissions) {
 
-        toast_sucess(`Bem vindo(a) ${response.email}`);
+      showToast(`Bem vindo(a) ${response.email}`, "success")
 
-        const user = {
-          id: response.id,
-          email: response.email,
-          permission: response.permissions
-        }
-
-        setData(user);
-
-          Redirect(response.permissions, router);
-
+      const user = {
+        id: response.id,
+        email: response.email,
+        permission: response.permissions
       }
+
+      setData(user);
+
+      Redirect(response.permissions, router);
+
     }
+  }
   }, [response])
 
-  return (
-    <form className={styles.box_form} onSubmit={handleSubmit(onSubmit)}>
-      <div className={styles.box_title}>
-        <Title class={styles.title_form} title="Login" fontFamily='var(--font-poppins)' fontWeight={700} />
-      </div>
+return (
+  <form className={styles.box_form} onSubmit={handleSubmit(onSubmit)}>
+    <div className={styles.box_title}>
+      <Title class={styles.title_form} title="Login" fontFamily='var(--font-poppins)' fontWeight={700} />
+    </div>
 
-      <div className={styles.box_input}>
-        <label className={styles.label_inputs}>{errors.email ? errors.email.message : ""}</label>
-        <Input
-          {...register("email")}
-          id={styles.input_email_login}
-          type="email"
-          placeholder={"E-mail"}
-          onChange={(e) => {
-            register("email").onChange(e);
-            setEmail(e.target.value)
-          }}
-          className={email ? errors.email ? styles.input_error : styles.input_ok : styles.input_ok}
-          name="email"
-          autoComplete="username"
-        />
-        <label className={styles.label_inputs}>{errors.password ? errors.password.message : ""}</label>
-        <Input
-          {...register("password")}
-          id={styles.input_senha_login}
-          type="password"
-          placeholder={"Senha"}
-          onChange={(e) => {
-            register("password").onChange(e);
-            setPassword(e.target.value)
-          }
-          }
-          className={password ? errors.password ? styles.input_error : styles.input_ok : styles.input_ok}
-          name="password"
-          autoComplete="current-password"
-          maxLength={25}
-        />
+    <div className={styles.box_input}>
+      <label className={styles.label_inputs}>{errors.email ? errors.email.message : ""}</label>
+      <Input
+        {...register("email")}
+        id={styles.input_email_login}
+        type="email"
+        placeholder={"E-mail"}
+        onChange={(e) => {
+          register("email").onChange(e);
+          setEmail(e.target.value)
+        }}
+        className={email ? errors.email ? styles.input_error : styles.input_ok : styles.input_ok}
+        name="email"
+        autoComplete="username"
+      />
+      <label className={styles.label_inputs}>{errors.password ? errors.password.message : ""}</label>
+      <Input
+        {...register("password")}
+        id={styles.input_senha_login}
+        type="password"
+        placeholder={"Senha"}
+        onChange={(e) => {
+          register("password").onChange(e);
+          setPassword(e.target.value)
+        }
+        }
+        className={password ? errors.password ? styles.input_error : styles.input_ok : styles.input_ok}
+        name="password"
+        autoComplete="current-password"
+        maxLength={25}
+      />
 
-      </div>
-      <div className={styles.checkbox_content}>
-        <input id={styles.checkbox} type="checkbox" name="remember" />
-        <p className={styles.label_checkbox}>Lembrar-me</p>
-      </div>
+    </div>
+    <div className={styles.checkbox_content}>
+      <input id={styles.checkbox} type="checkbox" name="remember" />
+      <p className={styles.label_checkbox}>Lembrar-me</p>
+    </div>
 
-      <div className={styles.button_content}>
-        <Button disabled={!email || !password || !!errors.email || !!errors.password} className={!email || !password || !!errors.email || !!errors.password ? styles.disabled_button : ""} text="avançar" />
-      </div>
+    <div className={styles.button_content}>
+      <Button disabled={!email || !password || !!errors.email || !!errors.password} className={!email || !password || !!errors.email || !!errors.password ? styles.disabled_button : ""} text="avançar" />
+    </div>
 
-      <div className={styles.box_hyperlink}>
-        <div className={styles.hyperlink_google}>
-          <img className={styles.img_google} src='/google_logo.png' alt="Google"></img>
-          <a id={styles.google_link} href="#">Faça login com o Google</a>
-        </div>
-        <a id={styles.password_link} href="/Recovery">
-          Esqueceu a senha/Primeiro acesso?
-        </a>
+    <div className={styles.box_hyperlink}>
+      <div className={styles.hyperlink_google}>
+        <img className={styles.img_google} src='/google_logo.png' alt="Google"></img>
+        <a id={styles.google_link} href="#">Faça login com o Google</a>
       </div>
-    </form>
-  );
+      <a id={styles.password_link} href="/Recovery">
+        Esqueceu a senha/Primeiro acesso?
+      </a>
+    </div>
+  </form>
+);
 }
