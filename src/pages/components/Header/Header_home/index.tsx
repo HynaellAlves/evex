8ad2b7066a0a5f80d3cfe82ad1e2a6d65c9d8662
EVEX <evex.noreply@gmail.com>
2025-07-31 15:29:52 -0300

    feat: Header Home

---
+++ src/pages/components/Header/Header_home/index.tsx
@@ -0,0 +1,33 @@
+import styles from './header_home.module.css';
+import { useRouter } from 'next/router';
+import Image from "@/pages/components/Image";
+import Link from 'next/link';
+
+export default function HeaderHome() {
+    const router = useRouter();
+    const path = router.pathname;
+
+    return (
+        <>
+        <header className={styles.header}>
+            <div className={styles.header_content}>
+                <Image href='' class={styles.logo_main} src="/logo_main.png" width={273} height={103} />
+                <div className={styles.header_navigation}>
+                    <Link href="/" className={`${styles.link} ${path === '/' ? styles.active : ''}`}>EVENTOS</Link>
+                    <Link href='#' className={styles.link}>SOBRE NÓS</Link>
+                    <Link href='#' className={styles.link}>CONTATO</Link>
+                </div>
+                <div className={styles.header_icons}>
+                    <div className={styles.content_icons}>
+                        <Image href='/Help' class={styles.header_icon} src="/accessibility_tools_icon.svg" width={46} height={46} />
+                        <Image href='/Acessibility' class={styles.header_icon} src="/help_icon.svg" width={46} height={46} />
+                    </div>
+                    <div className={styles.content_login}>
+                        <h1>LOGIN</h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+        </>
+    )
+}
