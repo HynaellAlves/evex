fc18e42c21bbea9d0e2bc838a331862d829cab2b
EVEX <evex.noreply@gmail.com>
2025-08-08 23:28:14 -0300

    feat: Events

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -8,30 +8,31 @@ import Input_search from '@/pages/components/Search_field';
 import { FiFilter, FiCalendar } from 'react-icons/fi';
 
 export default function NetxEvent() {
+
     return (
 
         <div id="page">
-            <Header/>
+            <Header />
             <div className={next.search_content}>
-            < Input_search />
+                < Input_search />
             </div>
             <div className={next.all_content}>
-            <h1 className={next.h1}>
+                <h1 className={next.h1}>
                     CONHEÇA OS PRÓXIMOS EVENTOS
-            </h1>
-            <div className={next.filters}>
-                <button id={next.filterIcon}  className={next.filterBtn}>
-                    <FiFilter className={next.filterIcon} />
-                    <span>Categoria</span>
-                </button>
-                <button className={next.filterBtn}>
-                    <FiCalendar className={next.filterIcon} />
-                    <span>Data</span>
-                </button>
+                </h1>
+                <div className={next.filters}>
+                    <button id={next.filterIcon} className={next.filterBtn}>
+                        <FiFilter className={next.filterIcon} />
+                        <span>Categoria</span>
+                    </button>
+                    <button className={next.filterBtn}>
+                        <FiCalendar className={next.filterIcon} />
+                        <span>Data</span>
+                    </button>
+                </div>
             </div>
-            </div>
-            <NetxEvents/>
-            <Footer/>
+            <NetxEvents />
+            <Footer />
         </div>
     );
 }
